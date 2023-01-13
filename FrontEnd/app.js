1c602be63e611b96bd63c248a4634a29a9d026d9
Jessy <jessegodais@gmail.com>
2023-01-13 18:17:40 +0100

    Etaps 1.1 faite

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -14,8 +14,7 @@ appelApi
             console.log(data[0].title);
             console.log(data[0].imageUrl);
 
-            // Création d'une boucle pour l'affichage de chaque élément dans le length
-            
+            // Création d'une boucle pour l'affichage de chaque élément dans le length   
             for (let i=0; i<data.length; i++) {
                 // Création d'un élément pour le départ
                 const figure = document.createElement("figure")
@@ -28,7 +27,63 @@ appelApi
                 const galleryEl = document.querySelector(".gallery")
                 galleryEl.appendChild(figure)
             }
+
+
+            // Programmation pour filtrer et afficher les travaux ciblés
+            
+            // Afficher tous les travaux
+            const allWorks = document.querySelector(".all_filter");
+            allWorks.addEventListener("click", function () {
+            const allWorksActive = data.filter(function (data) {
+                return data;
+            });
+            console.log(allWorksActive)
+            });
+
+            // Filtrer par objets
+            const objectsWorks = document.querySelector(".object_filter");
+            objectsWorks.addEventListener("click", function () {
+            const objectsWorksActive = data.filter(function (data) {
+                return data.category.name.includes("Objets");
+            });
+            console.log(objectsWorksActive)
+            });
+
+            // Afficher seulement les objets
+            const categorieWorks = data.map(data => data.category.name);
+            console.log(categorieWorks)
+            for(let i = data.length -1; i >= 0; i--) {
+                if(data[i].category.name.includes("Appartements" || "Hotels & restaurants")){
+                    categorieWorks.splice(i,1);
+                }
+            }
+
+
+            // Filtrer par Appartements
+            const apartmentsWorks = document.querySelector(".apartment_filter");
+            apartmentsWorks.addEventListener("click", function () {
+            const apartmentsWorksActive = data.filter(function (data) {
+                return data.category.name.includes("Appartements");
+            });
+            console.log(apartmentsWorksActive)
+            });
+            // Afficher seulement les appartements
+
+
+            // Filtrer par Hotels & restaurants
+            const hotelRestaurantsWorks = document.querySelector(".hotel_restaurant_filter");
+            hotelRestaurantsWorks.addEventListener("click", function () {
+            const hotelRestaurantsWorksActive = data.filter(function (data) {
+                return data.category.name.includes("Hotels & restaurants");
+            });
+            console.log(hotelRestaurantsWorksActive)
+            });
+            // Afficher seulement les Hotels & restaurants
+
+
         })
     })
     .catch((err) => console.log(err));
+
+
 
